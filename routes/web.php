5d6259803dfb560856bf366fc2664bb541321808
MikoFahreza <mikoyusrizal@gmail.com>
2023-04-06 10:40:45 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController; use Illuminate\Http\Request;
+
+Route::resource('mahasiswas', MahasiswaController::class);
+Route::post('cari', [MahasiswaController::class, 'cari'])->name('cari');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
